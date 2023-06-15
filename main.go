0a647f3606a4cffd113b908376b4da9971d84020
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-06-15 09:56:17 -0400

    perf: Have only one browser running and close it when mangal closes

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"github.com/metafates/mangal/cmd"
 	"github.com/metafates/mangal/config"
 	"github.com/metafates/mangal/log"
+	"github.com/metafates/mangal/provider/generic/headless"
 	"github.com/samber/lo"
 )
 
 func main() {
+	transport := headless.GetTransportSingleton()
+	defer func(transport *headless.Transport) {
+		_ = transport.Close()
+	}(transport)
 	lo.Must0(config.Setup())
 	lo.Must0(log.Setup())
 	cmd.Execute()
