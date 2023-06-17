26bc9536a37be34028e405a3b719b6f1f05ed754
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-06-17 18:22:32 -0400

    perf: Only try to cleanup transport if loaded

--- util/cleanup.go
+++ util/cleanup.go
@@ -5,6 +5,7 @@ import (
 	"os"
 )
 
+// Exit do any cleanup needed and exit the program with the given code
 func Exit(code int) {
 	if headless.IsLoaded() {
 		transport := headless.GetTransportSingleton()
