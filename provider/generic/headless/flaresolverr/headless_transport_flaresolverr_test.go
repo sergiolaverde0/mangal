517141898928056e3ecacc49e784174fd2fe2d34
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-07-20 15:53:58 -0400

    remove test

--- provider/generic/headless/flaresolverr/headless_transport_flaresolverr_test.go
+++ provider/generic/headless/flaresolverr/headless_transport_flaresolverr_test.go
@@ -1,46 +1,43 @@
 package flaresolverr
 
-import (
-	"context"
-	"github.com/belphemur/mangal/key"
-	"github.com/belphemur/mangal/util"
-	. "github.com/smartystreets/goconvey/convey"
-	"github.com/spf13/viper"
-	"net/http"
-	"testing"
-	"time"
-)
-
-func init() {
-	viper.Set(key.SourceHeadlessUseFlaresolverr, true)
-	viper.Set(key.SourceHeadlessFlaresolverrURL, "http://localhost:8191/v1")
-}
-
-func TestTransportFlaresolevrr(t *testing.T) {
-	if util.SkipCI(t) {
-		return
-	}
-
-	transport := NewTransport()
-
-	client := http.Client{
-		Transport: transport,
-	}
-	defer client.CloseIdleConnections()
-	Convey("Flaresolverr can go on ReaperScans", t, func() {
-		ctx, _ := context.WithTimeout(context.Background(), 120*time.Second)
-		req, err := http.NewRequestWithContext(ctx, "GET", "https://www.reaperscans.com", nil)
-		So(err, ShouldBeNil)
-
-		resp, err := client.Do(req)
-		So(err, ShouldBeNil)
-		defer resp.Body.Close()
-		So(resp.StatusCode, ShouldEqual, 200)
-
-		Convey("Flaresolverr session should be destroyed when finished", func() {
-			err := transport.Close()
-			So(err, ShouldBeNil)
-		})
-	})
-
-}
+//
+//import (
+//	"context"
+//	"github.com/belphemur/mangal/key"
+//	. "github.com/smartystreets/goconvey/convey"
+//	"github.com/spf13/viper"
+//	"net/http"
+//	"testing"
+//	"time"
+//)
+//
+//func init() {
+//	viper.Set(key.SourceHeadlessUseFlaresolverr, true)
+//	viper.Set(key.SourceHeadlessFlaresolverrURL, "http://localhost:8191/v1")
+//}
+//
+//func TestTransportFlaresolevrr(t *testing.T) {
+//
+//	transport := NewTransport()
+//
+//	client := http.Client{
+//		Transport: transport,
+//	}
+//	defer client.CloseIdleConnections()
+//	Convey("Flaresolverr can go on ReaperScans", t, func() {
+//		ctx, _ := context.WithTimeout(context.Background(), 120*time.Second)
+//		req, err := http.NewRequestWithContext(ctx, "GET", "https://www.reaperscans.com", nil)
+//		So(err, ShouldBeNil)
+//
+//		resp, err := client.Do(req)
+//		So(err, ShouldBeNil)
+//		defer resp.Body.Close()
+//		So(resp.StatusCode, ShouldEqual, 200)
+//
+//		Convey("Flaresolverr session should be destroyed when finished", func() {
+//			err := transport.Close()
+//			So(err, ShouldBeNil)
+//		})
+//	})
+//
+//}
