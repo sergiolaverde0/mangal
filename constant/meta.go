e0f472be35d412045663b2a882ff01465e6aae85
Antoine Aflalo <Belphemur@users.noreply.github.com>
2023-09-17 19:40:20 -0400

    release

--- constant/meta.go
+++ constant/meta.go
@@ -2,6 +2,6 @@ package constant
 
 const (
 	Mangal    = "mangal"
-	Version   = "4.5.8"
+	Version   = "4.5.9"
 	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 )
