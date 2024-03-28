30e5eb67fd5c195012e40015e879e4d071f0d45f
sergiolaverde0 <sergiolaverde0@noreply.codeberg.org>
2024-03-28 16:41:28 -0500

    feat: add adaptative colors for both light and dark terminals

--- color/color.go
+++ color/color.go
@@ -3,14 +3,17 @@ package color
 import "github.com/charmbracelet/lipgloss"
 
 var (
-    Red    = New("1")
-    Green  = New("2")
-    Yellow = New("3")
-    Blue   = New("4")
-    Purple = New("5")
-    Cyan   = New("6")
-    HiBg   = New("7")
-    HiFg   = New("10")
-    Fg     = New("11")
-    Bg     = New("15")
+    Red    = lipgloss.Color("1")
+    Green  = lipgloss.Color("2")
+    Yellow = lipgloss.Color("3")
+    Blue   = lipgloss.Color("4")
+    Purple = lipgloss.Color("5")
+    Cyan   = lipgloss.Color("6")
+)
+
+var (
+    HiBg   = lipgloss.AdaptiveColor{Light: "7", Dark: "0"}
+    HiFg   = lipgloss.AdaptiveColor{Light: "10", Dark: "14"}
+    Fg     = lipgloss.AdaptiveColor{Light: "11", Dark: "12"}
+    Bg     = lipgloss.AdaptiveColor{Light: "15", Dark: "8"}
 )
