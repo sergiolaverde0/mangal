40568ecaba8602b827b2519e7a909bf6fd4ad69e
Antoine Aflalo <Belphemur@users.noreply.github.com>
2024-08-23 10:21:21 -0400

    bump release

--- constant/meta.go
+++ constant/meta.go
@@ -2,6 +2,6 @@ package constant
 
 const (
 	Mangal    = "mangal"
-	Version   = "4.7.3"
+	Version   = "4.7.4"
 	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
 )
